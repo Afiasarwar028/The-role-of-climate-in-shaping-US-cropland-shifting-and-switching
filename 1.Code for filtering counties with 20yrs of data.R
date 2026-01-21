327491f7d98fddd7cdb18ef2bd50366ce4a8228a
Afiasarwar028 <asarwar@udel.edu>
2026-01-20 22:07:24 -0500

    Add files via upload
    Initial code of the analysis

---
+++ 1.Code for filtering counties with 20yrs of data.R
@@ -0,0 +1,35 @@
+install.packages("dplyr")
+library(dplyr)
+library(readxl)
+install.packages("writexl")
+library(writexl)
+
+# Assuming you've already loaded the data as 'data'
+barley <- "C:\\Users\\Folder\\UDel\\Research\\HarvestGRID_Data\\HG_original_Files\\Corn_IR.csv"
+data <- read.csv(barley)
+print(data)
+
+# Create a list of counties with at least 20 years of data
+counties_with_20_years_data <- data %>%
+  group_by(GEOID) %>%
+  filter(n_distinct(year) >= 20) %>%
+  ungroup() %>%
+  select(GEOID) %>%
+  distinct()
+
+# Filter the original data based on counties with 20 years of data
+filtered_original_data <- data %>%
+  semi_join(counties_with_20_years_data, by = ("GEOID"))
+
+# Print the filtered original data
+print(filtered_original_data)
+
+# Save the filtered original data to a new CSV file
+path <- "C:\\Users\\Folder\\UDel\\Research\\HarvestGRID_Data\\20_Years_filter\\Corn_IR.xlsx"
+write_xlsx(filtered_original_data, path)
+
+
+
+
+
+
